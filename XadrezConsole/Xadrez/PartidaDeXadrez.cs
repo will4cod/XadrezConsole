506a234f197a6b3be650f3d85abcd4e177c919f5
william <william4fernandes@gmail.com>
2019-11-24 16:46:20 -0300

    jogada especial promocao e finalizado

--- XadrezConsole/Xadrez/PartidaDeXadrez.cs
+++ XadrezConsole/Xadrez/PartidaDeXadrez.cs
@@ -145,6 +145,20 @@ namespace XadrezConsole.Xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("você não pode se colocar em xeque!");
             }
+            Peca p = tab.peca(destino);
+
+            // #jogadaespecial promocao
+            if(p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.Linha == 0) || p.cor == Cor.Preta && destino.Linha == 7)
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
@@ -166,7 +180,7 @@ namespace XadrezConsole.Xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             // #jogadaespecial en passant
             if(p is Peao && (destino.Linha == origem.Linha -2 || destino.Linha == origem.Linha + 2))
